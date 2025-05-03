ccc9674ff05c2817d09b2d1785209a1bfbc718c7
juanmig7890 <juanmig2708m@gmail.com>
2025-05-03 15:06:17 -0500

    Add files via upload

---
+++ mp.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+st.title("mi biografia")
+st.header("juan miguel cáceres")
+st.subheader("Bogotá:")
+st.write("Mi nombre es Juan Miguel Cáceres, tengo 17 años y soy de Bogotá, Colombia. Nací el 12 de septiembre de 2007. Viví en Bogotá gran parte de mi infancia, ya que allí residía toda mi familia y mis padres trabajaban en esa ciudad.Cursé prejardín y jardín en un lugar llamado Mis Angelitos Lindos, el cual me gustaba mucho a esa edad porque tenía una piscina de pelotas y muchos juegos con los que podía divertirme. Al llegar a la etapa de transición, me cambiaron a un colegio llamado Carmen Teresiano, que era bastante grande y tenía muchas más actividades y espacios que el jardín anterior.Sin embargo, solo estuve allí durante un año, ya que por motivos laborales de mi papá, nos mudamos a La Plata, Huila.")
+st.subheader("colegio teresiano:")
+st.image("teresiano_jpg.jpeg")
+
+st.subheader("La Plata, Huila:")
+st.write("La Plata ha sido el lugar donde he vivido los mejores momentos de mi vida, ya que allí tengo a mis mejores amigos, a quienes quiero como si fueran mis hermanos. Empecé primer grado en un colegio llamado Liceo Moderno Plateño. Lo que más me gustaba de ese colegio era el ambiente al aire libre y la comida, que era muy casera; especialmente el arroz, que me encantaba.En cuarto grado me trasladé a otro colegio, el Evita Rosso, donde terminé el bachillerato. Fue en este colegio donde conocí a mis mejores amigos y viví momentos únicos que nunca olvidaré. Sin embargo, las directivas no eran las mejores, ya que nos limitaban en muchas cosas y no nos permitían participar activamente en las actividades culturales del pueblo.En La Plata, me gusta mucho salir en moto y compartir con todos mis amigos, con quienes me gradué en diciembre de 2024. Después de la graduación, hicimos un viaje a Cartagena con algunos compañeros del salón, lo cual fue una experiencia inolvidable.Me interesan mucho los deportes. En mi último año escolar, representamos al departamento del Huila en las finales nacionales de balonmano, donde quedamos subcampeones. Gracias a este logro, recibimos un reconocimiento por dejar en alto la imagen del departamento.Después de todo lo vivido durante el colegio, sentí que era momento de tomar decisiones sobre mi futuro. Por eso opté por estudiar Ingeniería de Sistemas en Cali, ciudad donde también estudia mi hermana mayor.")
+st.subheader("viaje a cartagena:")
+st.video("cartagena_mp4.mp4")
+st.write("en este video podemos ver el viaje que relizamos a cartagena con los del salon y estabamos en unca chiva rumbera con unos señores de mexico")
+st.subheader("mi graduacion:")
+st.write("la siguiente imagen es una imagen con mis papás y mi hermana en mi graduacion")
+st.image("graduacion_jpg.jpg")
+
+
+
+
+
+
+
