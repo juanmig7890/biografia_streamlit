9b2af5c593b9016741457ccba5997bba41c68680
juanmig7890 <juanmig2708m@gmail.com>
2025-05-05 08:29:56 -0500

    Add files via upload

--- mp.py
+++ mp.py
@@ -1,31 +1,32 @@
 import streamlit as st
 import pandas as pd
-st.title("mi biografia")
-st.header("Juan Miguel Cáceres Catolico")
-st.subheader("Bogotá:")
-st.write("Mi nombre es Juan Miguel Cáceres Católico, tengo 17 años y nací Bogotá, Colombia. Nací el 12 de septiembre de 2007. Viví en Bogotá gran parte de mi infancia, ya que allí residía toda mi familia y mis padres trabajaban en esa ciudad.Cursé prejardín y jardín en un lugar llamado Mis Angelitos Lindos, el cual me gustaba mucho a esa edad porque tenía una piscina de pelotas y muchos juegos con los que podía divertirme. Al llegar a la etapa de transición, me cambiaron a un colegio llamado Carmen Teresiano, que era bastante grande y tenía muchas más actividades y espacios que el jardín anterior.Sin embargo, solo estuve allí durante un año, ya que por motivos laborales de mi papá, nos mudamos a La Plata, Huila.")
-st.subheader("colegio Carmen Teresiano:")
-st.image("teresiano_jpg.jpeg")
-
-st.subheader("La Plata, Huila:")
-st.write("La Plata ha sido el lugar donde he vivido los mejores momentos de mi vida, ya que allí tengo a mis mejores amigos, a quienes quiero como si fueran mis hermanos. Empecé primer grado en un colegio llamado Liceo Moderno Plateño. Lo que más me gustaba de ese colegio era el ambiente al aire libre y la comida, que era muy casera; especialmente el arroz, que me encantaba.En cuarto grado me trasladé a otro colegio, el Evita Rosso, donde terminé el bachillerato. Fue en este colegio donde conocí a mis mejores amigos y viví momentos únicos que nunca olvidaré. Sin embargo, las directivas no eran las mejores, ya que nos limitaban en muchas cosas y no nos permitían participar activamente en las actividades culturales del pueblo.En La Plata, me gusta mucho salir en moto y compartir con todos mis amigos, con quienes me gradué en diciembre de 2024. Después de la graduación, hicimos un viaje a Cartagena con algunos compañeros del salón, lo cual fue una experiencia inolvidable.Me interesan mucho los deportes. En mi último año escolar, representamos al departamento del Huila en las finales nacionales de balonmano, donde quedamos subcampeones. Gracias a este logro, recibimos un reconocimiento por dejar en alto la imagen del departamento.Después de todo lo vivido durante el colegio, sentí que era momento de tomar decisiones sobre mi futuro. Por eso opté por estudiar Ingeniería de Sistemas en Cali, ciudad donde también estudia mi hermana mayor.")
-st.subheader("mis mejores amigos:")
-st.write("ellos son mis mejores amigos y unas de las personas mas especiales e importantes en mi vida ya que me han enseñado muchas cosas y hemos pasado muchos momentos unicos")
-st.image("mejores amigos.jpg")
-st.subheader("viaje a cartagena:")
-st.video("cartagena_mp4.mp4")
-st.write("en este video podemos ver el viaje que relizamos a cartagena con los del salon y estabamos en unca chiva rumbera con unos señores de mexico")
-st.subheader("mi graduacion:")
-st.write("la siguiente imagen es una imagen con mis papás y mi hermana en mi graduacion")
-st.image("graduacion_jpg.jpg")
-st.subheader("torneo de balonmano:")
-st.write("el siguiente video es muy importante para mi ya que en ese momento pasamos a la final y mi hermana nos estaba apoyando.")
-st.video("paso_final.mp4")
-st.subheader("foto de todo el equipo con el director del inderhuila:")
-st.image("celebracion.jpg")
-st.subheader("mi vida contemporanea:")
-st.write("actualmente estoy viviendo en Cali con mi hermana mayor y estoy en primer semestre de ingenieria de sistemas que es una carrera muy linda y muy interesante. al pasar del tiempo he encontrado muy buenos amigos y he disfrutado todas las experiencias que me ha dado la universidad. ")
-st.image("amigos.jpg")
+izquierda,centro,derecha=st.columns(3)
+st.title("clientes")
+with izquierda:
+    nombre=st.text_input("nombre:")
+    apellidos=st.text_input("apellidos:")
+    edad=st.number_input_input("edad:")
+    if edad<18:
+        menor=("menor de edad")
+    else:
+        menor=("mayor de edad")
+with centro:
+    correo=st.text_input("correo:")  
+    telefono=st.text_input("telefono:")
+    direccion=st.text_input("direccion:")
+with derecha:
+    identificacion=st.number_input_input("identificacion:")
+    tipo=st.text_input("tipo de identificacion:")
+if st.button("guardar"):
+    st.success(f"nombre:{nombre}")
+    st.success(f"apellidos:{apellidos}")
+    st.success(f"edad:{edad}")
+    st.success(f"menor o mayor:{menor}")
+    st.success(f"correo:{correo}")
+    st.success(f"telefono:{telefono}")
+    st.success(f"direccion:{direccion}")
+    st.success(f"identificacion:{identificacion}")
+    st.success(f"tipo:{tipo}")
 
 
 
